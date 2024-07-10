b5253c4ab1be5aeb26419cf5fa448ada98ec0ef0
Luana-Nascimento <luananascimento00@outlook.com>
2024-07-10 00:23:58 -0300

    CRUD

--- FarmaLu/src/App.tsx
+++ FarmaLu/src/App.tsx
@@ -3,6 +3,10 @@ import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias';
+import FormularioCategoria from './components/categorias/formularioCategoria/FormularioCategoria';
+
+
 
 
 
@@ -15,6 +19,9 @@ function App() {
       <Routes> 
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/categorias" element={<ListaCategorias />} />
+        <Route path="/cadastroTema" element={<FormularioCategoria />} />
+
       </Routes>
     </div>
     <Footer/>
